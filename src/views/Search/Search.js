41088a7dbc97c49d246a2c990487780ed565b1d8
sarrthak008 <sarthaknavale08@gmail.com>
2024-09-08 10:05:59 +0530

    add axois and api call

--- src/views/Search/Search.js
+++ src/views/Search/Search.js
@@ -2,11 +2,39 @@ import React, { useState } from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './search.css'
 import { jobNames } from '../../config/searchData'
+import axios from 'axios'
+
+
+
+const searchJob = async (jobname)=>{
+    
+const options = {
+    method: 'GET',
+    url: 'https://job-search-api1.p.rapidapi.com/v1/job-description-search',
+    params: {
+      q: `${jobname}`,
+      page: '1',
+      country: 'us',
+      city: 'Seattle'
+    },
+    headers: {
+      'x-rapidapi-key': '81c0752aabmshd6f144b0b338b61p1ea32ejsn9eedaf578ced',
+      'x-rapidapi-host': 'job-search-api1.p.rapidapi.com'
+    }
+  };
+  
+  try {
+      const response = await axios.request(options);
+      console.log(response.data);
+  } catch (error) {
+      console.error(error);
+  }
+  
+}
 
 const Search = () => {
 
-    const [jobTittle, setjobTittle] = useState(null)
-
+    const [jobTittle, setjobTittle] = useState(null)     
 
   return (
     <div>
@@ -20,7 +48,7 @@ const Search = () => {
                  value={jobTittle} 
                  onChange={(e)=>{setjobTittle(e.target.value)}}>
                  </input>
-                 <button className='search-btn'><i class="ri-find-replace-line"></i></button>
+                 <button className='search-btn' onClick={()=>searchJob(jobTittle)}><i class="ri-find-replace-line"></i></button>
              </div>
              <div className='job-types-container'>
                  {jobNames.map((jobTitle)=>(
