6ba6b337ed3707e580e5c6ea619737ce5ff96480
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-09-08 16:14:46 +0530

    heading and buttons added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,24 +6,18 @@ import Footer from '../../components/Footer/Footer'
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
-
-      <div>
-        <div>
-          <h1>Empower Your Future - </h1>Explore Jobs, Apply, and Advance
-        </div>
-        <div>
-          <button type='button'></button>
+      <div className='content-container'>
+        <div className='content'>
+          <h3>Empower Your Future - Explore Jobs, Apply, and Advance</h3>
+          <div className='buttons'>
+            <button type='button'>Login</button>
+            <button type='button'>Signin</button>
+          </div>
         </div>
       </div>
-
-=======
-     <Navbar /> 
-      <h1>Home</h1>
->>>>>>> fc7734b7ed486ebd4d33cf58f54591acc7af0854
       <Footer />
-    </div>
+    </div >
   )
 }
 
