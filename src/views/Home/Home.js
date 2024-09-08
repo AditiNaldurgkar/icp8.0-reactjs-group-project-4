9d0ba5b9864387bf9aca5469ab26c39b59fca955
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-09-08 23:15:23 +0530

    css added to high demanded jobs

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,11 +6,11 @@ import job from './img/job.webp'
 import { toast } from 'react-hot-toast';
 
 const highDemandJobs = [
-  { title: 'Software Engineer', location: 'San Francisco, CA' },
-  { title: 'Data Scientist', location: 'New York, NY' },
-  { title: 'Product Manager', location: 'Remote' },
-  { title: 'UX Designer', location: 'Austin, TX' },
-  { title: 'Digital Marketer', location: 'Seattle, WA' },
+  { title: 'Software Engineer', description: 'Software Engineers design, develop, and maintain software applications and systems. They work on coding, testing, and debugging to ensure software is functional, reliable, and user-friendly.'},
+  { title: 'Data Scientist', description: ' Data Scientists analyze complex data sets to help organizations make data-driven decisions. They use statistical methods, machine learning, and data visualization to uncover insights and trends from data.' },
+  { title: 'Product Manager', description: 'Product Managers oversee the development and lifecycle of products. They work cross-functionally with engineering, marketing, and sales teams to define product features, develop roadmaps, and ensure successful product launches.' },
+  { title: 'UX Designer', description: ' UX Designers focus on creating intuitive and engaging user experiences for digital products. They conduct user research, create wireframes and prototypes, and collaborate with developers to ensure the product meets user needs and expectations.' },
+  { title: 'Digital Marketer', description: 'Digital Marketers develop and implement online marketing strategies to drive traffic and sales. They use various digital channels, including social media, email, and search engine marketing, to reach and engage target audiences.' },
 ];
 
 
@@ -41,15 +41,17 @@ function Home() {
         </div>
       </div>
       <div>
-        <h3>High-Demanded Jobs..</h3>
-        {highDemandJobs.map((job, index) => (
-          <div key={index} className='job-item'>
-          <h4>{job.title}</h4>
-          <p>{job.location}</p>
-          </div>
-        ))}
-      
+  <h3>High-Demanded Jobs..</h3>
+  <div className='job-list'>
+    {highDemandJobs.map((job, index) => (
+      <div key={index} className='job-item'>
+        <h4>{job.title}</h4>
+        <p>{job.description}</p>
       </div>
+    ))}
+  </div>
+</div>
+
       <Footer />
     </div >
   )
