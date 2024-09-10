fee8c7f16052fe5c96b1a2b8528025b6fef67542
MansiMali18 <mansimali399@gmail.com>
2024-09-10 08:42:55 +0530

    password and username feild added

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Login.css'
+import userimg from './user.png'
+import passwordimg from './padlock.png'
 function Login() {
   return (
     <>
@@ -16,6 +18,35 @@ function Login() {
 					Login to access data and information
 				</h6>
 			</div>
+			<form action="">
+				<div>
+					<label className="text-feild">Email</label>
+					<div className="input-with-icon">
+						<img
+							src={userimg}
+							alt="password icon"
+							className="icon"
+						/>
+						<input type="text" name="email" id="email" className="email" placeholder="Type your username" />
+					</div>
+				</div>
+				<div>
+					<label className="text-feild">Password</label>
+					<div className="input-with-icon">
+						<img
+							src={passwordimg} 
+							alt="password icon"
+							className="icon"
+						/>
+					<input type="password" name="passw" id="passw" className="passw" placeholder="Type your password" img src={userimg} />
+
+				</div>
+				</div>
+				<div className="text">
+					Forgot password ?
+				</div>
+				<button type="submit" className="login-btn">Login</button>
+			</form>
       </div>
       </div>
     </>
