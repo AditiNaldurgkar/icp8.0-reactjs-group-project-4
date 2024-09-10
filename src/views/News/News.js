3d17cfcc01b8e982e2b831f69065b59e6005df13
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-09-10 18:03:23 +0530

    discover added

--- src/views/News/News.js
+++ src/views/News/News.js
@@ -18,7 +18,7 @@ const newsData = [{
   url: 'https://img.freepik.com/free-photo/tech-startup-concept_23-2148588122.jpg?uid=R160163945&ga=GA1.1.1856927002.1722677193&semt=ais_hybrid',
     headline: "Tech Startups Surge in 2024",
     caption: 'Discover the top emerging startups making waves this year.',
-    url2 :'https://economictimes.indiatimes.com/tech/startups/economic-survey-india-saw-tech-startup-surge-post-covid-pandemic/articleshow/111931855.cms?from=mdr'
+      url2:"https://economictimes.indiatimes.com/tech/startups/economic-survey-india-saw-tech-startup-surge-post-covid-pandemic/articleshow/111931855.cms?from=mdr"
 },{
   url: 'https://img.freepik.com/free-photo/business-team-using-laptop-computer_1150-12568.jpg?uid=R160163945&ga=GA1.1.1856927002.1722677193&semt=ais_hybrid',
   headline: "Microsoft's New Cloud Services Expansion",
@@ -35,6 +35,7 @@ function News() {
           headline={item.headline}
           caption={item.caption}
           url={item.url}
+          url2={item.url2}
         />
       ))}
     </div>
