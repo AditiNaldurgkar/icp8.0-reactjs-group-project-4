50feb28a60c741b5f37137119bcd6b5c1f918175
MansiMali18 <mansimali399@gmail.com>
2024-09-10 08:37:07 +0530

    heading added into login page

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './Login.css'
 function Login() {
   return (
-    <div>Login</div>
+    <>
+    <div className="main-container">
+		<div className="left-container">
+			<div className="heading">
+				<h1>
+					Welcome back !!!!
+				</h1>
+				<h3>
+					Have an account ?
+				</h3>
+				<h6>
+					Login to access data and information
+				</h6>
+			</div>
+      </div>
+      </div>
+    </>
   )
 }
 
