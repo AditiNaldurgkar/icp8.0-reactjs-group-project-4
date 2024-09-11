2162ce9f08ac774d006531696f2cc2dd2c3472fb
RohitSanap07 <rohitsanap177@gmail.com>
2024-09-11 19:44:28 +0530

    Input handle added

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,1 +1,12 @@
 import React, { useState } from "react";
+
+onst userData = JSON.parse(localStorage.getItem("userData"));
+console.log(userData);
+
+
+function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
