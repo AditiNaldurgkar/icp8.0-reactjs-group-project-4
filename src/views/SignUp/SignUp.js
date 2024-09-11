c18bd3250aac54458a1b639ea673c466c7622d5d
RohitSanap07 <rohitsanap177@gmail.com>
2024-09-11 23:16:04 +0530

    form created

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -23,40 +23,53 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={handleSignUp}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
-  );
-}
+    <div className="signup-container">
+        <h2>Signup</h2>
+        <div className="form-group">
+            <label>Select Role:</label>
+            <select value={role} onChange={(e) => setRole(e.target.value)}>
+                <option value="Job Seeker">Job Seeker</option>
+                <option value="recruiter">Recruiter</option>
+            </select>
+        </div>
+        <div className="form-group">
+            <label>Username:</label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="form-group">
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        {role === 'recruiter' && (
+            <>
+                <div className="form-group">
+                    <label>Job Role Vacancy:</label>
+                    <input type="text" value={jobRole} onChange={(e) => setJobRole(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Company Name:</label>
+                    <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+                </div>
+            </>
+        )}
+        {role === 'Job Seeker' && (
+            <div className="form-group">
+                <label>Job Role Needed:</label>
+                <input type="text" value={jobRole} onChange={(e) => setJobRole(e.target.value)} />
+            </div>
+        )}
+        <div className="form-group">
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className="form-group">
+            <label>Confirm Password:</label>
+            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+        </div>
+        <button onClick={handleSignup} className="btn-signup">Sign Up</button>
+        <Toaster />
+    </div>
+);
+};
 
-export default SignUp;
+export default Signup;
