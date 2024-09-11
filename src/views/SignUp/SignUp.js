f72a9a0644ff758bb8cf3ae82dacd02395e3311a
RohitSanap07 <rohitsanap177@gmail.com>
2024-09-11 19:55:03 +0530

    Changes done

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -23,3 +23,42 @@ function SignUp() {
 
     console.log("User signed up and data stored in localStorage:", formData);
   };
+
+  return (
+    <form onSubmit={handleSignUp}>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default SignUp;
