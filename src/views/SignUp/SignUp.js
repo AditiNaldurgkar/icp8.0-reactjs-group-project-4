cbb72e2bbc1cdf69a201a8599fcc4188f2f42b5d
RohitSanap07 <rohitsanap177@gmail.com>
2024-09-12 18:08:07 +0530

    Sign up image added

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,26 +1,4 @@
-import React, { useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-import './SignUp.css';
 
-const Signup = () => {
-    const [role, setRole] = useState('Job Seeker');
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [jobRole, setJobRole] = useState('');
-    const [companyName, setCompanyName] = useState('');
-
-    const handleSignup = () => {
-      if (password !== confirmPassword) {
-          toast.error('Password do not match ❤️');
-          return;
-      }
-
-      const userData = { username, email, password, role, jobRole, companyName };
-      localStorage.setItem(username, JSON.stringify(userData));
-      toast.success(`${role.charAt(0).toUpperCase() + role.slice(1)} Signed up successfully 💚`);
-  };
 
   return (
     <div className="signup-container">
