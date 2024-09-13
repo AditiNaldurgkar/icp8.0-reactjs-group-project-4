e2df799e72f6e6ff0b17f7bec8aa4fe9bb7ea911
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-09-13 10:03:41 +0530

    add sidebar components

--- src/views/RecuriterProfile/RecuriterProfile.js
+++ src/views/RecuriterProfile/RecuriterProfile.js
@@ -1,20 +1,26 @@
 import React from "react";
 import './RecuriterProfile.css';
 import Form from '../.././components/Form/Form.js';
+import Sidebar from '../../components/Sidebar/Sidebar.js';
 
 const RecuriterProfile = () => {
 
     return (
         <>
+
             <div className="main">
+                <Sidebar />
                 <div className="frm-heading">
                     <p className="liner-text">Profile settings</p>
                     <p className="sub-heading">You can set perferref display name,create your profile URL and Manage other personal settings.</p>
+
                 </div>
 
                 <div className="form">
+
                     <Form />
                 </div>
+
             </div>
         </>
     )
