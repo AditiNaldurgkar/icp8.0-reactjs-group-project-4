c78960db4bed33a63b3fc529e58528fd371d17e9
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-09-13 10:19:27 +0530

    add Show div in file

--- src/views/RecuriterProfile/RecuriterProfile.js
+++ src/views/RecuriterProfile/RecuriterProfile.js
@@ -3,6 +3,7 @@ import './RecuriterProfile.css';
 import Form from '../.././components/Form/Form.js';
 import Sidebar from '../../components/Sidebar/Sidebar.js';
 
+
 const RecuriterProfile = () => {
 
     return (
@@ -13,14 +14,15 @@ const RecuriterProfile = () => {
                 <div className="frm-heading">
                     <p className="liner-text">Profile settings</p>
                     <p className="sub-heading">You can set perferref display name,create your profile URL and Manage other personal settings.</p>
-
                 </div>
 
                 <div className="form">
-
                     <Form />
                 </div>
 
+                <div className="Show">
+
+                </div>
             </div>
         </>
     )
