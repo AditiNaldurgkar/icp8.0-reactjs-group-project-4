40f594ffa6e0f82d8dcd4d3223a0f3d20ddce998
sejslsharmas <sejalsharmaany@gmail.com>
2024-09-15 22:01:53 +0530

    job detials from localstorage added

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -5,20 +5,39 @@ import Sidebar from '../../components/Sidebar/Sidebar'
 
 
 function UserProfile() {
- const [jobs, setappliedjobs] = useState([])
+ const [appliedjobs,setappliedjobs] = useState([]);
  useEffect(()=>{
-  const jobs = JSON.parse(localStorage.getItem('appliedjobs')) || [];
-  setappliedjobs(jobs)
- } , [])
+  const appliedjobs=JSON.parse(localStorage.getItem("appliedjobs"))|| [];
+  setappliedjobs(appliedjobs);
+ }, []);
+  
 
 
   return (
+    
     <div className='container'>
         <img src={UserProfileimg} className='img1'/>
+        <h4 className='head4'>Your Applied Jobs:</h4>
+       {
+        appliedjobs.map((appliedjob)=>{
+          const{title,description}=appliedjob;
+             return(
+              
+              <div className='jobs'> 
+              <span>Title : </span> {title}
+               <br></br>
+               <span className='jobdetails'>Job Description : </span> {description}
+              </div>
+             )
+})
+}
+       
+        <Sidebar/>
       
 
+<div className="line"></div>
       
-        <Sidebar/>
+       
         <div className='Show'></div>
 </div>
 )
