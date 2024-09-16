4473bf5f34482cb7395b8ccc663077249f599c87
prathamagre <prathamvaishu@gmail.com>
2024-09-16 12:29:32 +0530

    changes added

--- src/views/RecuriterProfileCard/RecuriterProfileCard.js
+++ src/views/RecuriterProfileCard/RecuriterProfileCard.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import "./RecuriterProfileCard.css"
+import UserProfileimg from "./../UserProfile/userprofileimg.png"
+
 
 function RecuriterProfileCard() {
-  return (
-    <div>
-        
-    </div>
-  )
+    const [items,setItems] = useState([]);
+
+    useEffect(()=>{
+        const savedProfileDetails = JSON.parse(localStorage.getItem("items")) || [];
+        setItems(savedProfileDetails);
+    },[])
+
+    return (
+        <div>
+            <Sidebar />
+            <div className='view-post-heading'>
+                <h2>Recruiter Profile</h2>
+            </div>
+            <div className='dtl-container'>
+                <img src={UserProfileimg} className='recruiter-profile-img' /><hr></hr>
+                <div className='recruiter-profile-details'>
+                    
+                </div>
+            </div>
+            <div className='Show'>
+            </div>
+        </div>
+    )
 }
 
 export default RecuriterProfileCard
