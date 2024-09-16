0aaa547e014d861418a84152e87fcdd050d2e358
sejslsharmas <sejalsharmaany@gmail.com>
2024-09-16 23:33:30 +0530

    images added

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import '../UserProfile/UserProfile.css'
 import UserProfileimg from '../../views/UserProfile/userprofileimg.png'
+import linkedinimg from '../../views/UserProfile/linkedinimg.png'
+import githubimg from '../../views/UserProfile/githubimg.png'
+import facebookimg from '../../views/UserProfile/facebook (1).png'
+import wave from '../../views/UserProfile/wave.png'
 import Sidebar from '../../components/Sidebar/Sidebar'
 
 
@@ -10,35 +14,45 @@ function UserProfile() {
   const appliedjobs=JSON.parse(localStorage.getItem("appliedjobs"))|| [];
   setappliedjobs(appliedjobs);
  }, []);
-  
+ 
+ /*const[user,setuserdetails] = useState([])
+useEffect(()=>{
+  const userDetails=JSON.parse(localStorage.getItem("LOGINUSER"))||[];
+  setuserdetails(userDetails);
+},[])*/
 
+const USER=JSON.parse(localStorage.getItem("LOGINUSER"))||[];
 
   return (
     
     <div className='container'>
         <img src={UserProfileimg} className='img1'/>
         <h4 className='head4'>Your Applied Jobs:</h4>
+        <span className='userdetails'>
+        <div className='username'><b>Hello {USER.username}!</b></div>
+        <div className='useremail'>{USER.email}</div>
+        </span>
+        
        {
         appliedjobs.map((appliedjob)=>{
           const{title,description}=appliedjob;
              return(
               
               <div className='jobs'> 
-              <span>Title : </span> {title}
+              <span className='title'>Title : {title} </span>
                <br></br>
                <span className='jobdetails'>Job Description : </span> {description}
               </div>
              )
 })
 }
-       
+
+
         <Sidebar/>
       
 
-<div className="line"></div>
-      
-       
-        <div className='Show'></div>
+<div className="verticalline"></div>
+<div className='Show'></div>
 </div>
 )
 }
