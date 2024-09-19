2774980d638577907657ed871c03eef9a2863c99
sarrthak008 <sarthaknavale08@gmail.com>
2024-09-19 07:20:19 +0530

    add add circle button

--- src/components/Usercard/Usercard.js
+++ src/components/Usercard/Usercard.js
@@ -9,7 +9,7 @@ const randonJobname =()=>{
 
 const Usercard = ({user}) => {
 
-    const {dob,cell,email,gender,name,login,phone,picture} = user
+    const {dob,cell,email,gender,name,login,phone,picture,location} = user
   return (
     <div className='user-info-card'>
        <div className='user-card-top'>
@@ -18,8 +18,18 @@ const Usercard = ({user}) => {
           </div>
           <div className='card-user-info'>
              <span className='user-card-name'>{`${name.first} ${name.last} .`}<span className='user-card-username'>@{login.username}</span></span>
-             <span>{randonJobname()}</span>
+             <span className='job-info'>{randonJobname()}</span>
           </div>
+       </div>
+       <div className='user-card-bottom'>
+          <div className='card-basic-info'>
+             <span> <i class="ri-cake-fill"></i>:{dob.age-10}</span>
+             <span><i class="ri-map-pin-2-fill"></i>:{(location.city).split(" ")[0]}</span>
+             <span>{gender == "male" ? <i class="ri-men-fill"></i> : <i class="ri-women-fill"></i>}</span>
+          </div>
+           <div className='card-btn-container'>
+              <button className='job-btn'>add to circle</button>
+           </div>
        </div>
     </div>
   )
