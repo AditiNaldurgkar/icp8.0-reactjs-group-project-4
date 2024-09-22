d7ca9d854629179e9bbd0da13031bf6cf2ac71e7
prathamagre <prathamvaishu@gmail.com>
2024-09-22 15:53:06 +0530

    changes adde

--- src/views/RecuriterProfileCard/RecuriterProfileCard.js
+++ src/views/RecuriterProfileCard/RecuriterProfileCard.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import "./RecuriterProfileCard.css";
 import UserProfileimg from "./../UserProfile/userprofileimg.png";
+import PhoneImg from "./RecuriterProfileCardImgs/telephone.png";
+import MailImg from "./RecuriterProfileCardImgs/mail (1).png";
+import LnkdinImg from "./RecuriterProfileCardImgs/linkedin.png"
+
 
 function RecuriterProfileCard() {
     const [items, setItems] = useState([]);
@@ -32,20 +36,22 @@ function RecuriterProfileCard() {
                                 alt="Recruiter Profile"
                             />
                             <div className='prfle-name'>
-                                 {profile.name}</div>
-                                 <br></br><hr></hr>
-                                 <p className='dtls'><b>Email:</b><a href={`mailto:${profile.email}`} className='clickable-info'>{profile.email}</a></p>
+                                {profile.name}</div>
 
-                                 <p className='dtls'><b>Mobile No:</b> <a href={`tel:${profile.mno}`} className='clickable-info'>{profile.mno}</a></p>
-                                 
-                                 <div className='dtls'><b>LinkedIn:</b> <a className="clickable-info" href={profile.linkedIn} target="_blank" rel="noopener noreferrer">{profile.linkedIn}</a></div>
+                            <br></br><hr></hr>
+
+                            <p className='dtls'><img src={PhoneImg} className='dtl-img' /><a href={`tel:${profile.mno}`} className='clickable-info'>+91{profile.mno}</a></p>
+
+                            <div className='dtls'><img src={LnkdinImg} className='dtl-img' /> <a className="clickable-info" href={profile.linkedIn} target="_blank" rel="noopener noreferrer">{profile.linkedIn}</a></div>
+
+                            <p className='dtls'><img src={MailImg} className='dtl-img' /><a href={`mailto:${profile.email}`} className='clickable-info'>{profile.email}</a></p>
                         </div>
-                        
+
                         <div className='recruiter-profile-details'>
-                        <hr></hr>
+                            <hr></hr>
                             <div className='dtls-left'>
 
-                                
+
                                 <p className='dtls'><b>Gender:</b> {profile.gender}</p>
                                 <p className='dtls'><b>Degree:</b> {profile.degree}</p>
                                 <p className='dtls'><b>Institute:</b> {profile.institude}</p>
@@ -59,8 +65,7 @@ function RecuriterProfileCard() {
                                 <div className='dtls'><b>Company:</b> {profile.company}</div>
                                 <div className='dtls'><b>Job:</b> {profile.job}</div>
                                 <div className='dtls'><b>Key Skill:</b> {profile.keyskill}</div>
-                                <div className='dtls'><b>LinkedIn:</b> <a className="lnkdintxt" href={profile.linkedIn} target="_blank" rel="noopener noreferrer">{profile.linkedIn}</a></div>
-                                
+
                             </div>
                         </div>
                     </div>
